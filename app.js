0f188a9b43e427166b6c8fb820aa568584cffd3a
cswat <connor.swatling+gh@gmail.com>
2019-08-08 13:07:24 -0600

    2019.08.08 update

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const paymentSelect = document.getElementById('payment')
 const creditCardEntry = document.getElementById('credit-card')
 const paypalText = document.getElementById('paypalDiv')
 const bitcoinText = document.getElementById('bitcoinDiv')
+const form = document.querySelector('form')
+const activitiesLabel = document.querySelectorAll('.activities input')
+const emailInput = document.getElementById('mail')
 
 //adding html label for total cost (calculated in event listener below)
 const costLabel = document.createElement('label')
@@ -67,7 +70,6 @@ designSelect.addEventListener('change', e => {
 
 //events that result from activity registration selections
 activitiesField.addEventListener('change', e => {
-    const activitiesLabel = document.querySelectorAll('.activities input')
     const date = e.target.attributes['data-day-and-time'].value
     if (e.target.tagName === 'INPUT') { //check for checkbox
         if (e.target.checked === true) {
@@ -117,3 +119,72 @@ paymentSelect.addEventListener('change', e => {
 //initializes payment screen without paypal or bitcoin
 hideElement(paypalText, true)
 hideElement(bitcoinText, true)
+
+//DRY for validation and error indication
+var validateField = (criteria, elementId) => {
+    element = document.getElementById(elementId)
+    if (criteria.test(element.value)) {
+        element.className = ''
+        return true
+    } else {
+        element.className = 'invalid-input'
+        return false
+    }
+}
+//validations
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    invalidResponse = 0
+    if (!validateField(/\w+/, 'name')) {
+        invalidResponse += 1
+    }
+    if (!validateField(/\S+@\S+\.\S+/, 'mail')) {
+        invalidResponse += 1
+    }
+    if (paymentSelect.value === 'credit card') {
+        if (!validateField(/\d{13,16}/, 'cc-num')) {
+            invalidResponse += 1
+        }
+        if (!validateField(/\d{5}/, 'zip')) {
+            invalidResponse += 1
+        }
+        if (!validateField(/\d{3}/, 'cvv')) {
+            invalidResponse += 1
+        }
+    } else if (paymentSelect.value === 'select method') {
+        invalidResponse += 1
+    }
+    activitiesField.className = 'invalid-input'
+    checked = 0
+    for (i = 0; i < activitiesLabel.length; i++) {
+        if (activitiesLabel[i].checked) {
+            checked += 1
+            activitiesField.className = ''
+        }
+    }
+    if (invalidResponse > 0 || checked === 0) {
+        e.preventDefault()
+    } else {
+        form.submit()
+    }
+});
+
+//real time error report
+emailInput.addEventListener('blur', e => {
+    if (validateField(/^$/, 'mail')) {
+        blankError = document.createElement('h2')
+        blankError.textContent = 'You need to add text!'
+        emailInput.parentNode.insertBefore(blankError, emailInput)
+        $(blankError).fadeOut(3000)
+    } else if (!validateField(/@/, 'mail')) {
+        atError = document.createElement('h2')
+        atError.textContent = 'You forgot an @'
+        emailInput.parentNode.insertBefore(atError, emailInput)
+        $(atError).fadeOut(3000)
+    } else if (!validateField(/\w+\.\S+/, 'mail')) {
+        domainError = document.createElement('h2')
+        domainError.textContent = 'Add a domain (.com/.net.edu)!'
+        emailInput.parentNode.insertBefore(domainError, emailInput)
+        $(domainError).fadeOut(3000)
+    }
+});
